108e5f88ae5d8cd52999fcc254cb0f0d2a1b7fc7
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2023-02-03 16:11:38 -0800

    .

--- build.ps1
+++
@@ -1,5 +0,0 @@
-# Remove-Item -Recurse -Force dist,build
-python3 setup.py bdist_wheel sdist --formats gztar
-pip uninstall -y mkdocs-file-filter
-pip install dist\mkdocs-file-filter-0.0.1.tar.gz
-# Remove-Item -Recurse -Force dist,build
