3f3deb942ecc17921727461ea37bb99f582b43c6
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2023-02-06 16:48:54 -0800

    Dev experience stuff (#10)

--- dev.build.sh
+++ dev.build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 rm -rf dist && rm -rf build && rm -rf site && rm -rf *.egg-info
+
+poetry run isort .
 poetry run black .
+poetry run flake8 --count .
+
 poetry build
+
 pip uninstall -y mkdocs_file_filter_plugin
 pip install dist/mkdocs_file_filter_plugin-*.tar.gz
+
+poetry run mkdocs serve --verbose
