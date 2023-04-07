1a45a8f8b1a8285f79005ebd6f149248a40e02f7
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2023-04-06 22:17:22 -0700

    tagging

--- dev.ps1
+++ dev.ps1
@@ -5,4 +5,4 @@ poetry run pre-commit autoupdate
 poetry run pre-commit run --all-files
 poetry self add "poetry-dynamic-versioning[plugin]"
 poetry build --verbose
-poetry run mkdocs build --verbose --config-file mkdocs.plugins.yml
+poetry run mkdocs serve --verbose --config-file mkdocs.plugins.yml
--- dev.sh
+++ dev.sh
@@ -6,4 +6,4 @@ poetry run pre-commit autoupdate
 poetry run pre-commit run --all-files
 poetry self add "poetry-dynamic-versioning[plugin]"
 poetry build --verbose
-poetry run mkdocs build --verbose --config-file mkdocs.plugins.yml
+poetry run mkdocs serve --verbose --config-file mkdocs.plugins.yml
