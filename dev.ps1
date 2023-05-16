94e52158852a3fa92174be9700cbad357fe17d07
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2023-05-15 18:26:57 -0700

    .

--- dev.ps1
+++ dev.ps1
@@ -1,8 +1,5 @@
 poetry poe clean-win
-poetry self add "poethepoet[plugin]"
-poetry poe pre-commit-update
 poetry poe pre-commit-all
-poetry self add "poetry-dynamic-versioning[plugin]"
 poetry poe test
 poetry poe build
 poetry poe mkdocs-serve --config-file mkdocs.plugins.yml
--- dev.sh
+++ dev.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 poetry poe clean-linux
-poetry self add "poethepoet[plugin]"
-poetry poe pre-commit-update
 poetry poe pre-commit-all
-poetry self add "poetry-dynamic-versioning[plugin]"
 poetry poe test
 poetry poe build
 poetry poe mkdocs-serve --config-file mkdocs.plugins.yml
