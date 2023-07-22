bf57c0267a50d74763551a41a16eeaad39890db9
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2023-07-22 10:46:06 -0700

    chore(deps): up

--- dev.ps1
+++ dev.ps1
@@ -2,4 +2,6 @@ poetry poe clean-win
 poetry poe pre-commit-all
 poetry poe test
 poetry poe build
-poetry poe mkdocs-serve --config-file mkdocs.plugins.yml
+Push-Location tests\fixtures
+poetry run mkdocs serve --config-file mkdocs.plugins.yml --verbose
+Pop-Location
